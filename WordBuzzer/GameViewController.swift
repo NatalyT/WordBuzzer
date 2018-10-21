978b511259187e48cc33e382a8680cfbe1d6ff54
Nataly <ngranetchi@gmail.com>
2018-10-21 11:32:57 +0200

    Add game screen and segue from welcome screen

---
+++ WordBuzzer/GameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameViewController.swift
+//  WordBuzzer
+//
+//  Created by Nataly Tatarintseva on 10/21/18.
+//  Copyright © 2018 Nataly Tatarintseva. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var playerFourButton: UIButton!
+    @IBOutlet weak var playerThreeButton: UIButton!
+    @IBOutlet weak var playerTwoButton: UIButton!
+    @IBOutlet weak var playerOneButton: UIButton!
+    @IBOutlet weak var labelOne: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playerOneButton.backgroundColor = .cyan
+        playerTwoButton.backgroundColor = .magenta
+        playerThreeButton.backgroundColor = .yellow
+        playerFourButton.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
