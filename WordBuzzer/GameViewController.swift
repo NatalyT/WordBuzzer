466ab51cb363155e2ccc55431faa2dd4e0f64bda
Nataly <ngranetchi@gmail.com>
2018-10-21 12:37:29 +0200

    Implement animation on game screen

--- WordBuzzer/GameViewController.swift
+++ WordBuzzer/GameViewController.swift
@@ -19,6 +19,8 @@ class GameViewController: UIViewController {
     @IBAction func unwindToGameScreen(segue:UIStoryboardSegue) {
     }
     
+    var labelTwo: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         playerOneButton.backgroundColor = .cyan
@@ -27,7 +29,34 @@ class GameViewController: UIViewController {
         playerFourButton.backgroundColor = .blue
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        let y = self.view.frame.height / 2 + 25
+        let x = -150
+        labelTwo = UILabel(frame: CGRect(x: x, y: Int(y), width: 150, height: 27))
+        labelTwo?.textAlignment = .right
+        labelTwo?.text = "LabelTwo"
+        labelTwo?.backgroundColor = .clear
+        labelTwo?.font = UIFont.systemFont(ofSize: 22)
+        labelTwo?.numberOfLines = 0
+        labelTwo?.lineBreakMode = .byWordWrapping
+        labelTwo?.sizeToFit()
+        self.view.addSubview(labelTwo!)
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        addAnimation()
+    }
+    
+    func addAnimation() {
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 5.0, delay: 0.0, options: .curveEaseInOut, animations: {
+                self.labelTwo?.center.x = self.view.frame.width + 150
+                self.view.layoutIfNeeded()
+            }, completion: { (finished: Bool) in
+                
+            })
+        }
+    }
     /*
      // MARK: - Navigation
      
